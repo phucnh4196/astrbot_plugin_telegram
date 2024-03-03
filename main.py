a2bd2113ecaff381fd94556e0135c68f9c39c31d
Phúc <phucnh4196@gmail.com>
2024-03-03 18:35:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class Main:
         image_path = None
         if isinstance(result.result_message, str):
             logging.info(f"telegram/{update.effective_chat.id} <- {result.result_message}")
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=escape_markdown(result.result_message), parse_mode='MarkdownV2')
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=self.escape_markdown(result.result_message), parse_mode='MarkdownV2')
             return
         for i in result.result_message:
             if isinstance(i, Plain):
